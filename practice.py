ffed9cc5b790b28bfd02f7ba0a9364adb0c9c553
Ahmed Ouaaz <ahmedouaaz@Ahmeds-MacBook-Pro.local>
2024-12-27 17:44:06 -0800

    Adding Optimzied moveZeros

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+import time
+
 class Solution:
     def moveZeros(self, nums: list[int]) -> list[int]:
         j = len(nums) - 1 
@@ -20,6 +22,17 @@ class Solution:
                 print(f"swapping number between i : {i} and number is {num} other j : {j} number is {nums[j]} ")
                 nums[i], nums[j] = nums[j], nums[i]
         return nums
+    
+    
+    def moveZeroes2(self, nums: list[int]) -> list[int]:
+        
+        first_index_non_zero = 0
+        for num in range(len(nums)):
+            if num != 0:
+                num , nums[first_index_non_zero] = nums[first_index_non_zero], num
+                first_index_non_zero += 1
+                
+        return nums 
             
     
     def majorityElements(self, nums: list[int]) -> int:
@@ -44,9 +57,22 @@ def main():
     num = [0,0,1,2,3,0,0]
     num = [0,0,1,2,0,0,0,3,0]
     test = Solution()
-    print(f"Original was  {num}")
-    result = test.moveZeroes(num)
+    start_time = time.time()
+    result = test.moveZeros(num)
     print(f"the output list is {result}")
+    end_time = time.time()
+    print(f"Original was  {num}")
+    print(f"elapsed time of moveZero1 : {end_time- start_time}")
+    start_time = time.time()
+    result = test.moveZeroes(num)
+    end_time = time.time()
+    print(f"the output list is {result}")
+    print(f"elapsed time of moveZero1 : {end_time- start_time}")
+    start_time = time.time()
+    result = test.moveZeroes2(num)
+    end_time = time.time()
+    print(f"the output list is {result}")
+    print(f"elapsed time of moveZero1 : {end_time- start_time}")
     
     # Finding majority of elements
     num = [2,2,2,1,1,1,2,2,2]
